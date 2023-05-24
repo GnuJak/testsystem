cf440ea7ff00339a088adad985a53a7023cc5450
John Hughes <djkno3@gmail.com>
2023-05-24 11:42:17 -0600

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const TESTSYSTEM = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ TESTSYSTEM.abilities = {
+  "str": "TESTSYSTEM.AbilityStr",
+  "dex": "TESTSYSTEM.AbilityDex",
+  "con": "TESTSYSTEM.AbilityCon",
+  "int": "TESTSYSTEM.AbilityInt",
+  "wis": "TESTSYSTEM.AbilityWis",
+  "cha": "TESTSYSTEM.AbilityCha"
+};
+
+TESTSYSTEM.abilityAbbreviations = {
+  "str": "TESTSYSTEM.AbilityStrAbbr",
+  "dex": "TESTSYSTEM.AbilityDexAbbr",
+  "con": "TESTSYSTEM.AbilityConAbbr",
+  "int": "TESTSYSTEM.AbilityIntAbbr",
+  "wis": "TESTSYSTEM.AbilityWisAbbr",
+  "cha": "TESTSYSTEM.AbilityChaAbbr"
+};
